910d1ae6f0b8e61548df11837a8d5c155866c8e8
github-action-benchmark <github@users.noreply.github.com>
2025-01-12 07:14:29 +0000

    add Codehigh Benchmark in TeX Live 2023 (customSmallerIsBetter) benchmark result for 16a781c1e7cc8d4711670f23392b95bcd24ce825

--- benchmark/2023/data.js
+++ benchmark/2023/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1736664409609,
+  "lastUpdate": 1736666069570,
   "repoUrl": "https://github.com/lvjr/codehigh",
   "entries": {
     "Codehigh Benchmark in TeX Live 2023": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 0.99769967109829,
             "unit": "ratio",
             "extra": "current time : previous time = 8.0607833862305 : 8.0793685913086"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tolvjr@163.com",
+            "name": "lvjr",
+            "username": "lvjr"
+          },
+          "committer": {
+            "email": "tolvjr@163.com",
+            "name": "lvjr",
+            "username": "lvjr"
+          },
+          "distinct": true,
+          "id": "16a781c1e7cc8d4711670f23392b95bcd24ce825",
+          "message": "remove outdated regex code (#18)",
+          "timestamp": "2025-01-12T14:53:40+08:00",
+          "tree_id": "dd3c4f55857d20ee1317785a68802dd29d8ecba7",
+          "url": "https://github.com/lvjr/codehigh/commit/16a781c1e7cc8d4711670f23392b95bcd24ce825"
+        },
+        "date": 1736666069157,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "compile codehigh with pdflatex",
+            "value": 0.95539931595161,
+            "unit": "ratio",
+            "extra": "current time : previous time = 191.90123748779 : 200.85971832275"
+          },
+          {
+            "name": "compile codehigh with lualatex",
+            "value": 0.65071807172377,
+            "unit": "ratio",
+            "extra": "current time : previous time = 5.4165954589844 : 8.3240280151367"
           }
         ]
       }
